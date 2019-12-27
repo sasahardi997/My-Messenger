7e3648eca367d138cb4e785bc71869129b89321d
Aleksandar Hardi <sasahardi@gmail.com>
2019-12-27 19:44:16 +0100

    first commit

---
+++ app/src/main/java/com/myappcompany/hardi/mymessenger/model/User.java
@@ -0,0 +1,52 @@
+package com.myappcompany.hardi.mymessenger.model;
+
+public class User {
+
+    private String id;
+    private String username;
+    private String imageURL;
+    private String search;
+
+    public User(){
+
+    }
+
+    public User(String id, String username, String imageURL, String search) {
+        this.id = id;
+        this.username = username;
+        this.imageURL = imageURL;
+        this.search=search;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
